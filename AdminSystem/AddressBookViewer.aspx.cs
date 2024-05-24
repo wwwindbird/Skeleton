6c0225de8069dbe464587d2f94f13c187092edd7
Feng Chen <fengchen@dmu.ac.uk>
2024-05-24 02:30:51 +0100

    Lab9

--- AdminSystem/AddressBookViewer.aspx.cs
+++ AdminSystem/AddressBookViewer.aspx.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        clsAddress AnAddress = new clsAddress();
+      
+        AnAddress = (clsAddress)Session["AnAddress"];
+        Response.Write(AnAddress.HouseNo);
 
     }
 }
